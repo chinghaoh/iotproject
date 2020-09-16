a514b1a0e8a8fa6cc2f9e97cc04afc057a3543a4
chinghao <chinghaoh@hotmail.nl>
2020-09-16 13:29:34 +0200

    Test file

---
+++ src/components/googlemaps.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Googlemaps extends Component{
+    render() {
+        return (
+            <div>
+                <p>Hello world</p>
+            </div>
+        );
+    }
+
+}
+
+export default Googlemaps;
---
+++ src/components/pagelayout.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Pagelayout extends Component{
+    render() {
+        return(
+            <div>
+                <p>"test</p>
+            </div>
+        )
+    }
+}
+
+export default Pagelayout;
